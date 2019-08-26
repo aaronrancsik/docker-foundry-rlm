91f9f7666f954d2576dbb84bf43a5fd20ba04b59
Aaron Rancsik <rancsikaron@stud.uni-obuda.hu>
2019-08-26 17:23:59 +0200

    volume map removed because we use fix cracked lic files

--- start.sh
+++ start.sh
@@ -3,8 +3,5 @@
 echo "IP ADDRESS:"
 hostname -i
 
-# Store the licese file inside the container for later reuse.
-cp /opt/rlm/licenses/foundry_float.lic /opt/foundry_float.lic
-
 # Run the license server directly with the copied license file.
 /usr/local/foundry/LicensingTools7.1/bin/RLM/rlm.foundry -c /opt/foundry_float.lic
